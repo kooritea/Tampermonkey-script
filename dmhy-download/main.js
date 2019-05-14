4872bfd24d3121d9f02e43b1ec1f86b84ed99e61
kooritea <ochatokori@gmail.com>
2019-05-14 17:08:52 +0800

    fix dmhy match

--- dmhy-download/main.js
+++ dmhy-download/main.js
@@ -4,31 +4,31 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        (https|http)://share.dmhy.org
+// @match        https://share.dmhy.org/*
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
 
 (function() {
-  'use strict';
-  function init(){
-      $("#topic_list tr .download-arrow").each(function(){
-          $(this).attr('href',"")
-          let url = $(this).parent().prev().children("a").attr("href")
-          $(this).attr("onclick","return false")
-          $(this).click(function(){
-              Download(url)
-              return false
-          })
-      })
-  }
-  function Download(url){
-      $.get(url,function(data){
-          let downurl = (data.match(/<strong>會員專用連接:<\/strong>&nbsp;<a href="(.*?)">/))[1]
-          let a = document.createElement('a')
-          a.href = downurl
-          a.click()
-      })
-  }
-  init()
-})();
+    'use strict';
+    function init(){
+        $("#topic_list tr .download-arrow").each(function(){
+            $(this).attr('href',"")
+            let url = $(this).parent().prev().children("a").attr("href")
+            $(this).attr("onclick","return false")
+            $(this).click(function(){
+                Download(url)
+                return false
+            })
+        })
+    }
+    function Download(url){
+        $.get(url,function(data){
+            let downurl = (data.match(/<strong>會員專用連接:<\/strong>&nbsp;<a href="(.*?)">/))[1]
+            let a = document.createElement('a')
+            a.href = downurl
+            a.click()
+        })
+    }
+    init()
+  })();
