8bb65b0f43cce9a31fbb70a148ff179b7af1b372
kooritea <ochatokori@gmail.com>
2023-01-22 23:02:43 +0800

    强制打开vue开发者工具添加对vue3的支持

--- force-vue-devtools/main.js
+++ force-vue-devtools/main.js
@@ -13,20 +13,25 @@
 
 (function () {
   'use strict';
+  if(!unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__){
+    return
+  }
   let vue
   const findVueEl = function () {
     const selectors = ['#app', '.app']
     for (const selector of selectors) {
       const el = unsafeWindow.document.querySelector(selector)
-      if (el && el.__vue__) {
-        vue = el.__vue__
+      if (el) {
+        vue = el.__vue__ || el.__vue_app__
+      }
+      if(vue){
         break
       }
     }
     if (!vue) {
       for (const el of unsafeWindow.document.body.children) {
-        if (el && el.__vue__) {
-          vue = el.__vue__
+        vue = el.__vue__ || el.__vue_app__
+        if(vue){
           break
         }
       }
@@ -40,20 +45,63 @@
       while (Vue.super) {
         Vue = Vue.super
       }
-      if (Vue.config.devtools === false && unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
-        Vue.config.devtools = true;
-        let hook = unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__
-        hook.emit('init', Vue)
-        if (vue.$store) {
-          let store = vue.$store;
-          store._devtoolHook = hook;
-          hook.emit('vuex:init', store);
-          hook.on('vuex:travel-to-state', function (targetState) {
-            store.replaceState(targetState);
-          });
-          store.subscribe(function (mutation, state) {
-            hook.emit('vuex:mutation', mutation, state);
-          });
+      const isVue3 = vue.version
+      const hook = unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__
+      if (!!(unsafeWindow.__NUXT__ || unsafeWindow.$nuxt)) {
+        let Vue
+        if (unsafeWindow.$nuxt) {
+          Vue = unsafeWindow.$nuxt.$root && unsafeWindow.$nuxt.$root.constructor
+        }
+        if (Vue.config.devtools != true) {
+          Vue.config.devtools = true
+          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
+        }
+        unsafeWindow.postMessage(
+          {
+            devtoolsEnabled: true,
+            vueDetected: true,
+            nuxtDetected: true,
+          },
+          "*"
+        )
+        return
+      }
+      if(isVue3){
+        if (unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled === false) {
+          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.apps.push({
+            app: vue,
+            version: vue.version,
+            types: {
+              Comment: Symbol("Comment"),
+              Fragment: Symbol("Fragment"),
+              Static: Symbol("Static"),
+              Text: Symbol("Text"),
+            },
+          })
+          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled = true
+          unsafeWindow.postMessage(
+            {
+              devtoolsEnabled: true,
+              vueDetected: true,
+            },
+            "*"
+          )
+        }
+      }else{
+        if (Vue.config.devtools === false) {
+          Vue.config.devtools = true;
+          hook.emit('init', Vue)
+          if (vue.$store) {
+            let store = vue.$store;
+            store._devtoolHook = hook;
+            hook.emit('vuex:init', store);
+            hook.on('vuex:travel-to-state', function (targetState) {
+              store.replaceState(targetState);
+            });
+            store.subscribe(function (mutation, state) {
+              hook.emit('vuex:mutation', mutation, state);
+            });
+          }
         }
       }
     } else {
