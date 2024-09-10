006e0568bcec1dd2961604ca1c16dd674d038b7f
kooritea <ochatokori@gmail.com>
2024-09-10 11:55:03 +0800

    [force-vue-devtools]添加vue3版本nuxt的支持

--- force-vue-devtools/main.js
+++ force-vue-devtools/main.js
@@ -10,97 +10,99 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-
-(function () {
-  'use strict';
-  if(!unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__){
-    return
-  }
+(function() {
+  'use strict'
   let vue
-  const findVueEl = function () {
+  const findVueEl = function() {
+    if (!unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
+      setTimeout(() => {
+        findVueEl()
+      }, 1000)
+      return
+    }
     const selectors = ['#app', '.app']
     for (const selector of selectors) {
       const el = unsafeWindow.document.querySelector(selector)
       if (el) {
         vue = el.__vue__ || el.__vue_app__
       }
-      if(vue){
+      if (vue) {
         break
       }
     }
     if (!vue) {
       for (const el of unsafeWindow.document.body.children) {
         vue = el.__vue__ || el.__vue_app__
-        if(vue){
+        if (vue) {
           break
         }
       }
     }
     patchVue()
   }
-  const patchVue = function () {
+  const patchVue = function() {
     if (vue) {
-      let constructor = vue.__proto__.constructor
-      let Vue = constructor;
+      const constructor = vue.__proto__.constructor
+      let Vue = constructor
       while (Vue.super) {
         Vue = Vue.super
       }
       const isVue3 = vue.version
+      if (isVue3 ? vue.config.devtools : Vue.config.devtools) {
+        return
+      }
+      console.log('try open vue devtools')
+      if (isVue3) {
+        vue.config.devtools = true
+      } else {
+        unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
+        Vue.config.devtools = true
+      }
       const hook = unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__
-      if (!!(unsafeWindow.__NUXT__ || unsafeWindow.$nuxt)) {
-        let Vue
-        if (unsafeWindow.$nuxt) {
-          Vue = unsafeWindow.$nuxt.$root && unsafeWindow.$nuxt.$root.constructor
-        }
-        if (Vue.config.devtools != true) {
-          Vue.config.devtools = true
-          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
-        }
+      if (unsafeWindow.__NUXT__ || unsafeWindow.$nuxt) {
         unsafeWindow.postMessage(
           {
             devtoolsEnabled: true,
             vueDetected: true,
-            nuxtDetected: true,
+            nuxtDetected: true
           },
-          "*"
+          '*'
         )
-        return
       }
-      if(isVue3){
-        if (!unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled) {
-          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.apps.push({
-            app: vue,
-            version: vue.version,
-            types: {
-              Comment: Symbol("Comment"),
-              Fragment: Symbol("Fragment"),
-              Static: Symbol("Static"),
-              Text: Symbol("Text"),
-            },
-          })
-          unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled = true
-          unsafeWindow.postMessage(
-            {
-              devtoolsEnabled: true,
-              vueDetected: true,
-            },
-            "*"
-          )
-        }
-      }else{
+      if (isVue3) {
+        unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled = true
+        unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.apps.push({
+          app: vue,
+          version: vue.version,
+          types: {
+            Comment: Symbol('Comment'),
+            Fragment: Symbol('Fragment'),
+            Static: Symbol('Static'),
+            Text: Symbol('Text')
+          }
+        })
+        unsafeWindow.__VUE_DEVTOOLS_GLOBAL_HOOK__.enabled = true
+        unsafeWindow.postMessage(
+          {
+            devtoolsEnabled: true,
+            vueDetected: true
+          },
+          '*'
+        )
+      } else {
         if (Vue.config.devtools === false) {
-          Vue.config.devtools = true;
+          Vue.config.devtools = true
           hook.emit('init', Vue)
           if (vue.$store) {
-            let store = vue.$store;
-            store._devtoolHook = hook;
-            hook.emit('vuex:init', store);
-            hook.on('vuex:travel-to-state', function (targetState) {
-              store.replaceState(targetState);
-            });
-            store.subscribe(function (mutation, state) {
-              hook.emit('vuex:mutation', mutation, state);
-            });
+            const store = vue.$store
+            store._devtoolHook = hook
+            hook.emit('vuex:init', store)
+            hook.on('vuex:travel-to-state', function(targetState) {
+              store.replaceState(targetState)
+            })
+            store.subscribe(function(mutation, state) {
+              hook.emit('vuex:mutation', mutation, state)
+            })
           }
         }
       }
@@ -111,4 +113,4 @@
     }
   }
   findVueEl()
-})();
+})()
